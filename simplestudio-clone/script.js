27ed1e21d10e2bb403906fb8cbefdd7e1fc1c686
Mona Raj <agrmonaraj@gmail.com>
2025-01-20 23:23:32 +0530

    build people section,  manifesto section and move gsap cdn to head of html

--- simplestudio-clone/script.js
+++ simplestudio-clone/script.js
@@ -1,17 +1,10 @@
-// const logoSVG = document.querySelector("svg g path");
-
-// const color = logoSVG.getAttribute("fill");
-
-// console.log(logoSVG);
-
-// logoSVG.setAttribute("fill", "#cd1111")
-
+// hide or unhide details in services section
 const servicesItems = document.getElementsByClassName("service-item-name");
-console.log(servicesItems)
+// console.log(servicesItems)
 Array.from(servicesItems).forEach((value) => {
     value.addEventListener("click", (e) => {
         let item = e.target;
-        console.log(item);
+        // console.log(item);
         let details = item.parentNode.parentNode.children[1];
         if (details.style.display === "block") {
             details.style.display = "none";
@@ -20,5 +13,21 @@ Array.from(servicesItems).forEach((value) => {
         }
     })
 })
+
+// change text opacity on scroll in manifesto section
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".text-change", {
+    opacity: 1,
+    stagger: 2,
+    scrollTrigger: {
+      trigger: ".section-manifesto",
+      start: "top 20%",
+      end: "bottom 80%",
+      scrub: true,
+      markers: true
+    },
+  })
+
 
 
